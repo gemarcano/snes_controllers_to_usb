71071d24f36a3073cddaeab1c51e1e6b55dc0a5c
Gabriel Marcano <gabemarcano@yahoo.com>
2024-03-06 01:38:59 -0800

    Refactor watchdog implementation
     - Use better FreeRTOS function to create tasks (includes core affinity)
     - Use atomic_bool instead of atomic_int as we're treating it as a bool
       anyway
     - Make sure lines aren't too long

--- firmware/src/watchdog.cpp
+++ firmware/src/watchdog.cpp
@@ -18,18 +18,19 @@
 namespace sctu
 {
 	constexpr const int cpu_cores = 2;
-	static std::array<std::atomic_int, cpu_cores> watchdog_cpu_status;
+	static std::array<std::atomic_bool, cpu_cores> watchdog_cpu_status;
 	static std::array<const char*, cpu_cores> watchdog_task_names {
-		"watchdog_cpu0",
-		"watchdog_cpu1"
+		"sctu_watchdog_cpu0",
+		"sctu_watchdog_cpu1"
 	};
 
 	void watchdog_cpu_task(void* status)
 	{
 		for(;;)
 		{
-			std::atomic_int &status_ = *reinterpret_cast<std::atomic_int*>(status);
-			status_ = 1;
+			std::atomic_bool &status_ =
+				*reinterpret_cast<std::atomic_bool*>(status);
+			status_ = true;
 			vTaskDelay(50);
 		}
 	}
@@ -42,7 +43,10 @@ namespace sctu
 		watchdog_enable(200, true);
 		for(;;)
 		{
-			if (std::ranges::all_of(watchdog_cpu_status, [](int val){return !!val;}))
+			// The compiler is apparently smart enough to optimize this by
+			// unrolling the range check
+			constexpr const auto value = [](bool val){return val;};
+			if (std::ranges::all_of(watchdog_cpu_status, value))
 			{
 				watchdog_update();
 				std::ranges::fill(watchdog_cpu_status, 0);
@@ -53,9 +57,6 @@ namespace sctu
 
 	void initialize_watchdog_tasks()
 	{
-		// Initialize watchdog hardware
-		TaskHandle_t handle;
-
 		// Watchdog priority is higher
 		// Dedicated watchdog tasks on each core, and have a central watchdog task
 		// aggregate the watchdog flags from both other tasks.
@@ -63,10 +64,22 @@ namespace sctu
 		// watchdog, or it will itself be hung, leading to a system reset.
 		for (size_t i = 0; i < cpu_cores; ++i)
 		{
-			xTaskCreate(watchdog_cpu_task, watchdog_task_names[i], configMINIMAL_STACK_SIZE, &watchdog_cpu_status[i], tskIDLE_PRIORITY+2, &handle);
-			vTaskCoreAffinitySet(handle, (1 << i) );
+			xTaskCreateAffinitySet(
+				watchdog_cpu_task,
+				watchdog_task_names[i],
+				configMINIMAL_STACK_SIZE,
+				&watchdog_cpu_status[i],
+				tskIDLE_PRIORITY+2,
+				1 << i,
+				nullptr);
 		}
-		xTaskCreate(watchdog_task, "watchdog_core", configMINIMAL_STACK_SIZE, nullptr, tskIDLE_PRIORITY+2, &handle);
-		vTaskCoreAffinitySet(handle, (1 << 0) | (1 << 1) );
+		xTaskCreateAffinitySet(
+			watchdog_task,
+			"sctu_watchdog_core",
+			configMINIMAL_STACK_SIZE,
+			nullptr,
+			tskIDLE_PRIORITY+2,
+			(1 << 0) | (1 << 1),
+			nullptr);
 	}
 }
